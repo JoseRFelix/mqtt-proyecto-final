bb4b935dc4d196ecf951eacaa174058a2ef3dd65
JoseRFelix <jfelx@outlook.com>
2020-12-19 16:31:27 -0400

    feat: counter

--- packages/counter/App.js
+++ packages/counter/App.js
@@ -1,17 +1,77 @@
 import * as React from "react";
-import { View, StyleSheet, Animated } from "react-native";
+import { View, StyleSheet, Animated, Text } from "react-native";
 import Constants from "expo-constants";
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import mqtt from "@taoqf/react-native-mqtt";
+import * as Speech from "expo-speech";
+
+function getClient() {
+  return mqtt.connect("ws://r124c4c7.en.emqx.cloud:8083/mqtt", {
+    username: "counter",
+    password: "test",
+    clientId: "counter",
+  });
+}
 
 export default function App() {
+  const [duration, setDuration] = React.useState(null);
+  const client = React.useRef(null);
+
+  React.useEffect(() => {
+    client.current = getClient();
+    client.current.on("connect", () => {
+      client.current.subscribe("start:counter", (err) => {
+        console.log(err);
+      });
+    });
+
+    client.current.on("message", (topic, message) => {
+      console.log(`Received message from topic: ${topic}`);
+      const parsedMessage = JSON.parse(message.toString());
+
+      if (parsedMessage && parsedMessage.duration) {
+        setDuration(parsedMessage.duration);
+      }
+    });
+
+    return () => client.current.end();
+  }, [setDuration, duration]);
+
+  React.useEffect(() => {
+    let intervalId;
+    if (duration > 0) {
+      intervalId = setTimeout(() => setDuration(duration - 1), 1000);
+    }
+
+    if (duration <= 0 && duration !== null) {
+      setDuration(null);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [setDuration, duration]);
+
+  React.useEffect(() => {
+    if (duration <= 10 && duration !== null) {
+      Speech.speak(duration.toString());
+    }
+  });
+
+  if (!duration) {
+    return (
+      <View style={styles.container}>
+        <Text>Waiting for Traffic Light...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <CountdownCircleTimer
         isPlaying
-        duration={10}
+        duration={duration}
         colors="#004777"
         onComplete={() => {
-          console.log("ON_COMPLETE BEFORE RETURN");
+          setDuration(null);
           return [true, 0];
         }}
       >
--- packages/traffic-light/src/App.js
+++ packages/traffic-light/src/App.js
@@ -4,10 +4,13 @@ import mqtt from "mqtt";
 
 import "./App.css";
 
-const client = mqtt.connect("ws://r124c4c7.en.emqx.cloud:8083/mqtt", {
-  username: "test",
-  password: "test",
-});
+function getClient() {
+  return mqtt.connect("ws://r124c4c7.en.emqx.cloud:8083/mqtt", {
+    username: "traffic-light",
+    password: "test",
+    clientId: "traffic-light",
+  });
+}
 
 const TIME_TO_RED = 3;
 const DELAY_TO_RED = 5;
@@ -22,24 +25,26 @@ function App() {
   const [counter, setCounter] = React.useState(null);
   const [timeToYellow, setTimeToYellow] = React.useState(null);
   const [timeToRed, setTimeToRed] = React.useState(null);
+  const client = React.useRef(null);
 
   React.useEffect(() => {
-    client.on("connect", () => {
-      client.subscribe("put:traffic", (err) => {});
+    client.current = getClient();
+    client.current.on("connect", () => {
+      client.current.subscribe("put:traffic", (err) => {});
     });
 
-    client.on("message", (topic, message) => {
-      console.log(topic, message.toString());
+    client.current.on("message", (topic, message) => {
+      console.log(`Received message from topic: ${topic}`);
 
       if (timeToYellow !== null) {
         return setTimeToYellow((timeToYellow) => timeToYellow - 1);
       }
-      console.log("hello");
-      changeToRed(DELAY_TO_RED);
 
-      client.end();
+      changeToRed(DELAY_TO_RED);
     });
-  }, [timeToYellow]);
+
+    return () => client.current.end();
+  }, [setTimeToYellow, timeToYellow]);
 
   // TO YELLOW
   React.useEffect(() => {
@@ -48,7 +53,7 @@ function App() {
       intervalId = setTimeout(() => setTimeToYellow(timeToYellow - 1), 1000);
     }
 
-    if (timeToYellow <= 0) {
+    if (timeToYellow <= 0 && timeToYellow !== null) {
       setState((state) => ({ ...state, GreenOn: false, YellowOn: true }));
       setTimeToYellow(null);
       setTimeToRed(TIME_TO_RED);
@@ -64,12 +69,17 @@ function App() {
       intervalId = setTimeout(() => setTimeToRed(timeToRed - 1), 1000);
     }
 
-    if (timeToRed <= 0) {
+    if (timeToRed <= 0 && timeToRed !== null) {
       setState((state) => ({ ...state, YellowOn: false, RedOn: true }));
 
       if (counter === null) {
         setCounter(COUNTER_TIME);
-        client.publish("start:counter", COUNTER_TIME.toString());
+
+        console.log("sending message to topic: start:counter");
+        const payload = {
+          duration: COUNTER_TIME,
+        };
+        client.current.publish("start:counter", JSON.stringify(payload));
       }
 
       setTimeToRed(null);
@@ -85,7 +95,7 @@ function App() {
       intervalId = setTimeout(() => setCounter(counter - 1), 1000);
     }
 
-    if (counter <= 0) {
+    if (counter <= 0 && counter !== null) {
       setState((state) => ({ ...state, RedOn: false, GreenOn: true }));
       setCounter(null);
     }
@@ -95,11 +105,6 @@ function App() {
 
   const changeToRed = (delay) => {
     setTimeToYellow(delay);
-  };
-
-  const changeToGreen = () => {
-    // TODO: dynamic value
-    setState((state) => ({ ...state, GreenOn: true, RedOn: false }));
   };
 
   return (
